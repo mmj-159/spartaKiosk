82ec36c3be1bb283f6a2a0b064b0dd3982bef314
mtm159 <myoungminjun@gmail.com>
2025-01-16 20:52:14 +0900

    2차등록

--- src/main/java/com/example/kiosk/Kiosk.java
+++ src/main/java/com/example/kiosk/Kiosk.java
@@ -1,5 +1,40 @@
 package com.example.kiosk;
 
-public class Kiosk {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Kiosk {
+    List<MenuItem> menuItems = new ArrayList<>();
+
+    public Kiosk(List<MenuItem> _menuItems){
+        this.menuItems = _menuItems;
+    }
+
+    public void start(){
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            for (int i = 0; i < menuItems.size(); i++) {
+                MenuItem item = menuItems.get(i); // List에서 객체를 가져옴
+                System.out.println((i+1) + ". " +
+                        item.getMenuName() +
+                        " |  W " + item.getMenuPrice() +
+                        "|  " + item.getMenuDescription());
+            }
+            System.out.println("0. 종료      | 종료");
+
+            int choice = sc.nextInt();
+            if (choice == 1) {
+                System.out.println("\n");
+                //System.out.println("선택한 메뉴 : " + shackBurger.getMenuName() + "| 가격:" + shackBurger.getMenuPrice() + "| 설명:" + shackBurger.getMenuDescription());
+                System.out.println("\n");
+            } else if (choice == 0) {
+                System.out.println("프로그램이 종료됩니다");
+                break;
+            } else {
+                System.out.println("유효하지 않은 선택입니다. 다시 시도하세요.");
+            }
+        }
+    }
 }
--- src/main/java/com/example/kiosk/Main.java
+++ src/main/java/com/example/kiosk/Main.java
@@ -13,46 +13,11 @@ public class Main {
         MenuItem hamburger = new MenuItem("Hamburger", 5.4,"비프패티를 기반으로 야채가 들어간 기본버거");
 
         List<MenuItem> menuItems = new ArrayList<>();
+
         menuItems.add(shackBurger);
         menuItems.add(smokeShack);
         menuItems.add(cheeseburger);
         menuItems.add(hamburger);
 
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            for (int i = 0; i < menuItems.size(); i++) {
-                MenuItem item = menuItems.get(i); // List에서 객체를 가져옴
-                System.out.println((i+1) + ". " +
-                        item.getMenuName() +
-                        " |  W " + item.getMenuPrice() +
-                        "|  " + item.getMenuDescription());
-            }
-            System.out.println("0. 종료      | 종료");
-
-            int choice = sc.nextInt();
-            if (choice == 1) {
-                System.out.println("\n");
-                System.out.println("선택한 메뉴 : " + shackBurger.getMenuName() + "| 가격:" + shackBurger.getMenuPrice() + "| 설명:" + shackBurger.getMenuDescription());
-                System.out.println("\n");
-            } else if (choice == 2) {
-                System.out.println("\n");
-                System.out.println("선택한 메뉴 : " + smokeShack.getMenuName() + "| 가격:" + smokeShack.getMenuPrice() + "| 설명:" + smokeShack.getMenuDescription());
-                System.out.println("\n");
-            } else if (choice == 3) {
-                System.out.println("\n");
-                System.out.println("선택한 메뉴 : " + cheeseburger.getMenuName() + "| 가격:" + cheeseburger.getMenuPrice() + "| 설명:" + cheeseburger.getMenuDescription());
-                System.out.println("\n");
-            } else if (choice == 4) {
-                System.out.println("\n");
-                System.out.println("선택한 메뉴 : " + hamburger.getMenuName() + "| 가격:" + hamburger.getMenuPrice() + "| 설명:" + hamburger.getMenuDescription());
-                System.out.println("\n");
-            } else if (choice == 0) {
-                System.out.println("프로그램이 종료됩니다");
-                break;
-            } else {
-                System.out.println("유효하지 않은 선택입니다. 다시 시도하세요.");
-            }
-        }
     }
 }
